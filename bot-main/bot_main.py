8e35cccc80fb674e882a0286ba6840ea8bdbe280
bigr4232 <ryanmf4232@gmail.com>
2023-10-08 01:18:05 -0700

    Implement extra commands

--- bot-main/bot_main.py
+++ bot-main/bot_main.py
@@ -2,6 +2,7 @@ import discord
 from discord import app_commands
 import config_loader
 import command_sender
+import asyncio
 
 # Intents and tree inits
 intents = discord.Intents.default()
@@ -10,6 +11,7 @@ client = discord.Client(intents=intents)
 managers = {}
 tree = app_commands.CommandTree(client)
 config = config_loader.loadYaml()
+gamemode = 'nade-practice'
 
 # Checks if user has the role with a role ID as input
 async def checkIfUserHasRole(roles, roleID):
@@ -31,29 +33,72 @@ async def syncCommands(ctx: discord.Interaction):
 @tree.command(name='start-server', description='send command to server to start', guild=discord.Object(id=config['discordGuildID']))
 async def startServerCommand(ctx: discord.Interaction):
     if await checkIfUserHasRole(ctx.user.roles, config['discordAdminRole']):
-        await ctx.response.send_message('Starting Counter Strike server.')
+        await ctx.response.send_message('Starting Counter Strike server.', delete_after=30)
         await command_sender.startServer(config['startCommand'])
     else:
-        await ctx.response.send_message('This command must be run by a Counter Strike server admin.')
+        await ctx.response.send_message('This command must be run by a Counter Strike server admin.', delete_after=30)
 
 # Stop server command
 @tree.command(name='stop-server', description='send command to server to stop', guild=discord.Object(id=config['discordGuildID']))
 async def startServerCommand(ctx: discord.Interaction):
     if await checkIfUserHasRole(ctx.user.roles, config['discordAdminRole']):
-        await ctx.response.send_message('Stopping Counter Strike server.')
-        await command_sender.startServer(config['stopCommand'])
+        await ctx.response.send_message('Stopping Counter Strike server.', delete_after=30)
+        await command_sender.stopServer()
     else:
-        await ctx.response.send_message('This command must be run by a Counter Strike server admin.')
+        await ctx.response.send_message('This command must be run by a Counter Strike server admin.', delete_after=30)
         
 # Restart server command
 @tree.command(name='restart-server', description='send command to server to restart', guild=discord.Object(id=config['discordGuildID']))
 async def startServerCommand(ctx: discord.Interaction):
     if await checkIfUserHasRole(ctx.user.roles, config['discordAdminRole']):
-        await ctx.response.send_message('Restarting Counter Strike server.')
-        await command_sender.startServer(config['stopCommand'])
+        await ctx.response.send_message('Restarting Counter Strike server.', delete_after=30)
+        await command_sender.stopServer()
         await command_sender.startServer(config['startCommand'])
     else:
-        await ctx.response.send_message('This command must be run by a Counter Strike server admin.')
+        await ctx.response.send_message('This command must be run by a Counter Strike server admin.', delete_after=30)
+
+# Start gamemode
+@tree.command(name='gamemode', description='start gamemode on the server specified by the option', guild=discord.Object(id=config['discordGuildID']))
+@app_commands.choices(option=[app_commands.Choice(name='nade-practice', value='nade-practice')])
+async def serverGameModeCommand(ctx: discord.Interaction, option:app_commands.Choice[str]):
+    if await checkIfUserHasRole(ctx.user.roles, config['discordAdminRole']):
+        await ctx.response.send_message(f'Switching server to gamemode {option.value}', delete_after=30)
+        global gamemode
+        gamemode = 'nade-practice'
+        await command_sender.gamemodeStart(option.value)
+    else:
+        await ctx.response.send_message('This command must be run by a Counter Strike server admin.', delete_after=30)
+
+# Change map
+@tree.command(name='changemap', description='changemap to specified map', guild=discord.Object(id=config['discordGuildID']))
+@app_commands.choices(option=[app_commands.Choice(name='ancient', value='ancient'),
+                              app_commands.Choice(name='anubis', value='anubis'),
+                              app_commands.Choice(name='dust2', value='dust2'),
+                              app_commands.Choice(name='inferno', value='infero'),
+                              app_commands.Choice(name='mirage', value='mirage'),
+                              app_commands.Choice(name='nuke', value='nuke'),
+                              app_commands.Choice(name='overpass', value='overpass'),
+                              app_commands.Choice(name='vertigo', value='vertigo')])
+async def changeMap(ctx: discord.Interaction, option:app_commands.Choice[str]):
+    if await checkIfUserHasRole(ctx.user.roles, config['discordAdminRole']):
+        await ctx.response.send_message(f'Switching server to the map {option.value}', delete_after=30)
+        await command_sender.changemap(option.value)
+        await asyncio.sleep(10)
+        await command_sender.gamemodeStart(gamemode)
+    else:
+        await ctx.response.send_message('This command must be run by a Counter Strike server admin.', delete_after=30)
+
+# Send server command
+@tree.command(name='sendServerCommand', description='Send a command to the server', guild=discord.Object(id=config['discordGuildID']))
+async def sendServerCommand(ctx: discord.Interaction, command: str):
+    if await checkIfUserHasRole(ctx.user.roles, config['discordAdminRole']):
+        await ctx.response.send_message(f'Sending command to server {command}', delete_after=30)
+        resp = await command_sender.sendCMD(command)
+        if resp != '':
+            await ctx.channel.send(f'Server responded with {resp}')
+    else:
+        await ctx.response.send_message('This command must be run by a Counter Strike server admin.', delete_after=30)
+
 
 # Initialization
 @client.event
--- bot-main/command_sender.py
+++ bot-main/command_sender.py
@@ -2,7 +2,7 @@ import subprocess
 
 # Initalize a command
 async def initCommand(inputCommand):
-    inputCommand = '\'' + inputCommand + '^M\''
+    inputCommand = inputCommand + '^M'
     command = ['screen', '-S', 'csgoServer', '-X', 'stuff', inputCommand]
     return command
 
@@ -15,3 +15,20 @@ async def startServer(startCommand):
 # Send terminal command to stop server
 async def stopServer():
     subprocess.run(['screen', '-S', 'csgoServer', '-X', 'quit'])
+
+# Send command to execute certain gamemode
+async def gamemodeStart(gamemode):
+    if gamemode == 'nade-practice':
+        cmd = await initCommand('exec nadeprac')
+    subprocess.run(cmd)
+
+# Send changelevel command to server
+async def changemap(map):
+    cmd = await initCommand(f'changelevel de_{map}')
+    subprocess.run(cmd)
+
+# Send command to server and return response
+async def sendCMD(command):
+    cmd = await initCommand(command)
+    response = subprocess.run(cmd, capture_output=True)
+    return response

3e8c94351d8248d18f0cb0e385a6124d6ac154c7
bigr4232 <ryanmf4232@gmail.com>
2023-11-23 22:33:32 -0600

    Add and get server from database implemented

--- discord-bot/bot-main/test_pytest.py
+++ discord-bot/bot-main/test_pytest.py
@@ -1,7 +1,13 @@
 import pytest
 import command_sender
+import databaseHandler
+import asyncio
 
 @pytest.mark.asyncio
 async def testNumPlayers():
     numplayers = await command_sender.getNumPlayers()
     return
+
+@pytest.mark.asyncio
+async def testAddServer():
+    await databaseHandler.addServer('70.1.1.1', 'Arizona', 'bigr4232')

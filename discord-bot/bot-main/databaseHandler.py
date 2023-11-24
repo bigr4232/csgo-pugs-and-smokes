e0a471511f77d4532d55d703b5c7b7afa5448840
bigr4232 <ryanmf4232@gmail.com>
2023-11-23 22:33:32 -0600

    Add and get server from database implemented

--- discord-bot/bot-main/databaseHandler.py
+++ discord-bot/bot-main/databaseHandler.py
@@ -1,1 +1,45 @@
-import psycopg2s
+import psycopg2
+import config_loader
+from state_to_abbrevation import stateToAbbrev
+
+config = config_loader.loadYaml()
+dbname = config['dbserversname']
+host = config['dbhost']
+user = config['dbuser']
+password = config['dbpassword']
+
+#conn = psycopg2.connect(host=host, user=user, password=password, dbname=dbname)
+#cur = conn.cursor()
+
+#cur.execute('INSERT INTO serverlist VALUES (%s, %s, %s, %s)', ('70.190.28.30', 'ryan-IL', 'Illinois', '***REMOVED***'))
+
+#cur.execute("SELECT * FROM serverlist;")
+#test = cur.fetchone()
+
+#conn.commit()
+
+#cur.close()
+#conn.close()
+
+# Add server to database
+async def addServer(ip, location, discordName, port):
+    with psycopg2.connect(host=host, user=user, password=password, dbname=dbname) as conn:
+        with conn.cursor() as cur:
+            cur.execute('SELECT * FROM serverlist;')
+            servers = cur.fetchall()
+            numServers = 0
+            for server in servers:
+                if server[2] == location and server[3] == discordName:
+                    numServers += 1
+            serverid = f'{discordName}-{stateToAbbrev[location]}'
+            if numServers > 0:
+                serverid += f'-{str(numServers)}'
+            cur.execute('INSERT INTO serverlist VALUES (%s, %s, %s, %s, %s)', (ip, serverid, location, discordName, port))
+            conn.commit()
+
+# Get list of servers in database
+async def getServers():
+    with psycopg2.connect(host=host, user=user, password=password, dbname=dbname) as conn:
+        with conn.cursor() as cur:
+            cur.execute('SELECT * FROM serverlist;')
+    return cur.fetchall()

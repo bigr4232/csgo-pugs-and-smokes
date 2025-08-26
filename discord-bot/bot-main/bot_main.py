bd45eab6cad34d1b4ab111730a93c1abbc7c00e7
Ryan Finley <ryanmf4232@gmail.com>
2025-08-25 19:58:54 -0700

    Change version to 2.1.1

--- discord-bot/bot-main/bot_main.py
+++ discord-bot/bot-main/bot_main.py
@@ -14,7 +14,7 @@ import datetime
 from concurrent.futures import ThreadPoolExecutor
 
 # Intents, tree inits, globals
-__version__ = '2.1.0'
+__version__ = '2.1.1'
 __requiredControllerVersion__ = '1.1.0'
 intents = discord.Intents.default()
 intents.message_content = True
